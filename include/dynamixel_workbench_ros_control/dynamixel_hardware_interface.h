ed749a8e6ad3e2c8b6e581a359769339836dd808
Martin Oehler <martin.sven.oehler@gmail.com>
2017-06-29 17:41:00 +0200

    initial commit

---
+++ include/dynamixel_workbench_ros_control/dynamixel_hardware_interface.h
@@ -0,0 +1,48 @@
+#ifndef DYNAMIXEL_HARWARE_INTERFACE_H
+#define DYNAMIXEL_HARWARE_INTERFACE_H
+
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+
+#include <ros/ros.h>
+
+#include <dynamixel_workbench_toolbox/dynamixel_multi_driver.h>
+
+namespace dynamixel_workbench_ros_control {
+
+class DynamixelHardwareInterface : public hardware_interface::RobotHW
+{
+public:
+  DynamixelHardwareInterface();
+
+  bool init();
+  void read();
+  void write();
+
+private:
+  bool loadDynamixels(ros::NodeHandle& nh);
+  void setTorque(bool enabled);
+
+  double convertValue2Radian(uint32_t value);
+
+  boost::shared_ptr<dynamixel_multi_driver::DynamixelMultiDriver> driver_;
+
+  hardware_interface::JointStateInterface jnt_state_interface;
+  hardware_interface::PositionJointInterface jnt_pos_interface;
+  std::vector<std::string> joint_names_;
+
+  std::vector<double> goal_position_;
+  std::vector<double> goal_effort_;
+  std::vector<double> goal_velocity_;
+
+  std::vector<double> current_position_;
+  std::vector<double> current_velocity_;
+  std::vector<double> current_effort_;
+};
+
+}
+
+
+
+#endif
---
+++ src/dynamixel_hardware_interface.cpp
@@ -0,0 +1,96 @@
+#include <dynamixel_workbench_ros_control/dynamixel_hardware_interface.h>
+
+namespace dynamixel_workbench_ros_control {
+
+
+DynamixelHardwareInterface::DynamixelHardwareInterface() {
+
+
+
+}
+
+bool DynamixelHardwareInterface::init() {
+  // Load dynamixel config from parameter server
+  ros::NodeHandle pnh("~");
+  if (!loadDynamixels(pnh)) {
+    return false;
+  }
+  // initialize sync read/write
+  driver_->initSyncRead();
+  driver_->initSyncWrite();
+
+  // register interfaces
+  // connect and register the joint state interface
+  for (unsigned int i = 0; i < joint_names_.size(); i++) {
+    hardware_interface::JointStateHandle state_handle(joint_names_[i], &current_position_[i], &current_velocity_[0], &current_effort_[i]);
+    jnt_state_interface.registerHandle(state_handle);
+
+    hardware_interface::JointHandle pos_handle(state_handle, &goal_position_[i]);
+    jnt_pos_interface.registerHandle(pos_handle);
+  }
+  registerInterface(&jnt_state_interface);
+  registerInterface(&jnt_pos_interface);
+
+  setTorque(true);
+}
+
+bool DynamixelHardwareInterface::loadDynamixels(ros::NodeHandle& nh) {
+  ROS_INFO_STREAM("Loading parameters from namespace " << nh.getNamespace() + "/dynamixels");
+
+  std::string port_name;
+  nh.getParam("dynamixels/port_info/port_name", port_name);
+  int baudrate;
+  nh.getParam("dynamixels/port_info/baudrate", baudrate);
+  float protocol_version;
+  nh.getParam("dynamixels/port_info/protocol_version", protocol_version);
+
+  driver_.reset(new dynamixel_multi_driver::DynamixelMultiDriver(port_name, baudrate, protocol_version));
+
+  std::vector<dynamixel_driver::DynamixelInfo*> infos;
+  XmlRpc::XmlRpcValue dxls;
+  nh.getParam("dynamixels/device_info", dxls);
+  ROS_ASSERT(dxls.getType() == XmlRpc::XmlRpcValue::TypeStruct);
+  for(XmlRpc::XmlRpcValue::ValueStruct::const_iterator it = dxls.begin(); it != dxls.end(); ++it) {
+    std::string dxl_name = (std::string)(it->first);
+    joint_names_.push_back(dxl_name);
+    ros::NodeHandle dxl_nh(nh, "dynamixels/device_info/" + dxl_name);
+
+    dynamixel_driver::DynamixelInfo * info = new dynamixel_driver::DynamixelInfo;
+    int model_id;
+    dxl_nh.getParam("id", model_id);
+    info->model_id = model_id;
+    int model_number;
+    dxl_nh.getParam("model_number", model_number);
+    info->model_number = model_number;
+    infos.push_back(info);
+  }
+
+  driver_->loadDynamixel(infos);
+  for (unsigned int i = 0; i < infos.size(); i++) {
+    delete infos[i];
+  }
+}
+
+void DynamixelHardwareInterface::setTorque(bool enabled) {
+  std::vector<uint8_t> torque(joint_names_.size(), enabled);
+  driver_->syncWriteTorque(torque);
+}
+
+
+void DynamixelHardwareInterface::read() {
+  driver_->syncReadPosition(current_position_);
+  // implement new driver function for radians
+}
+
+void DynamixelHardwareInterface::write() {
+  driver_->syncWritePosition(goal_position_);
+  // implement new driver function for radians
+}
+
+double DynamixelHardwareInterface::convertValue2Radian(uint32_t value)
+{
+  // look how its done for thor
+  return 0.0;
+}
+
+}
